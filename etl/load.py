f0fe7609e89f6722fd7c3d45df81791f78ce1dab
Overseek <imanblmkdm@gmail.com>
2023-06-26 16:26:35 +0100

    comment added

--- etl/load.py
+++ etl/load.py
@@ -4,5 +4,7 @@ import pandas as pd
 
 @task(log_prints=True)
 def load_data(dataframe, database, name):
+
+    # loading to database:
     engine = create_engine(database)
     dataframe.to_sql(con=engine, name=name, if_exists='append', index=False)
